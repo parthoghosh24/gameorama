adf6382f5f36ae7932a97ce4c37623a78da506b9
Partho Ghosh <defjam24@gmail.com>
2014-04-15 22:15:09 +0530

    Application helper updated to handle Page Titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+ def fullTitle(pageTitle)
+    baseTitle = "Gameorama- The ultimate gaming platform"
+    if pageTitle.empty?
+       baseTitle
+    else
+      "#{baseTitle} | #{pageTitle}"
+    end
+  end
 end
