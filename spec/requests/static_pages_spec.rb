2dcb0fbc42e8f2dc80b326ae3f9aa42968e13598
Partho Ghosh <defjam24@gmail.com>
2014-04-22 11:45:06 +0530

    Added a failing test for a non-existent page 'Location'. Now will create this page and make the test pass

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,9 +3,22 @@ require 'spec_helper'
 describe "Static Pages" do
   describe "Home Page" do
     it "should have the content 'Log in to'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/' # using capybara
+      visit '/' # using capybara's visit
       expect(page).to have_content('Log in to')
+    end
+  end
+
+  describe "About Page" do
+      it "should have the content 'Gameorama has been developed'" do
+         visit '/about'
+         expect(page).to have_content('Gameorama has been developed')
+      end
+  end
+
+  describe "Location" do
+    it "should have the content 'You can locate us at'" do
+       visit '/location'
+       expect(page).to have_content('You can locate us at') 
     end
   end
 end
